c21d4901d83bb79a98d0e67d9043c3b15e9d7805
Your Name <you@example.com>
2023-10-20 08:29:24 +0700

    message

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std ;
+int main(){
+  int n , arr[1000] ; 
+  int max = arr[0];
+    for(int i = 0; i < n ; i++){
+        cin >> arr[i] ;
+      if(arr[i] != 0 )
+          cout << arr[i] << endl ;
+      else 
+          break ;
+   
+  }
+}
